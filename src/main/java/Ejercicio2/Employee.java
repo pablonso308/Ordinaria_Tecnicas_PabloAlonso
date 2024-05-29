8271833e6ab48763617a50c44022ebd3255c9d2c
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-05-29 16:52:54 +0200

    implemento clases de seguimiento y formacion

---
+++ src/main/java/Ejercicio2/Employee.java
@@ -0,0 +1,42 @@
+package Ejercicio2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private String id;
+    private List<Course> enrolledCourses;
+    private AcademicRecord academicRecord;
+
+    public Employee(String name, String id) {
+        this.name = name;
+        this.id = id;
+        this.enrolledCourses = new ArrayList<>();
+        this.academicRecord = new AcademicRecord();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Course> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public AcademicRecord getAcademicRecord() {
+        return academicRecord;
+    }
+
+    public void enrollCourse(Course course) {
+        enrolledCourses.add(course);
+    }
+
+    public double calculateAverageGrade() {
+        return academicRecord.calculateAverageGrade();
+    }
+}
