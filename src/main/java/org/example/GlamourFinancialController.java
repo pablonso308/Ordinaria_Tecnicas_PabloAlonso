170e4e9d1ec2d2ec3129454d6ff0d9ccabd38bba
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-05-29 16:48:52 +0200

    implemento la vista y el controlador

---
+++ src/main/java/org/example/GlamourFinancialController.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// GlamourFinancialController.java
+public class GlamourFinancialController {
+    private GlamourFinancial model;
+    private GlamourFinancialView view;
+
+    public GlamourFinancialController(GlamourFinancial model, GlamourFinancialView view) {
+        this.model = model;
+        this.view = view;
+        this.view.addUpdateButtonListener(new UpdateButtonListener());
+        this.view.addCalculateButtonListener(new CalculateButtonListener());
+    }
+
+    class UpdateButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String market = view.getMarket();
+            double price = view.getPrice();
+            model.updateStockPrice(market, price);
+            view.setOutput("Updated price for " + market + ": " + price);
+        }
+    }
+
+    class CalculateButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            double marketValue = model.calculateMarketValue();
+            double priceToBookValue = model.calculatePriceToBookValue();
+            view.setOutput("Market Value: " + marketValue + "\nPrice-to-Book Value: " + priceToBookValue);
+        }
+    }
+}
