ee1f0e087cb738a385754a32485170366dc04f3d
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-05-29 16:52:40 +0200

    implemento clases de seguimiento y formacion

---
+++ src/main/java/Ejercicio2/Course.java
@@ -0,0 +1,25 @@
+package Ejercicio2;
+
+public class Course {
+    private String title;
+    private Subject subject;
+    private String instructor;
+
+    public Course(String title, Subject subject, String instructor) {
+        this.title = title;
+        this.subject = subject;
+        this.instructor = instructor;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+}
